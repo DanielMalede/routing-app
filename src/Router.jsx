89d74b96491e36c1ae81343168c90629afc02434
DanielMalede <danielmalede@gmail.com>
2022-08-25 16:11:12 +0300

    .

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,7 +2,6 @@ import { NotFound, Home, Main } from "./components/pages/index";
 import { Routes, Route } from "react-router-dom";
 import Devices from "./components/contexts/devices";
 
-
 export default function Router() {
   return (
     <Devices>
