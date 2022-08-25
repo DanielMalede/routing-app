2971224afe02b6b57bcad2a7de921a4303e265e6
DanielMalede <danielmalede@gmail.com>
2022-08-25 16:10:51 +0300

    .

--- src/components/contexts/devices.jsx
+++ src/components/contexts/devices.jsx
@@ -1,13 +1,14 @@
 import { createContext, useState, useEffect } from "react";
-import {devicesApi} from "../../services/devicesAPI";
-
+import { devicesApi } from "../../services/devicesAPI";
 
 export const devicesContext = createContext("Ma Nishma");
 
 export default function Devices({ children }) {
   const [devices, setDevices] = useState([]);
   useEffect(() => {
-    devicesApi().then((res)=>{setDevices(res)})
+    devicesApi().then((res) => {
+      setDevices(res);
+    });
   }, []);
 
   return (
--- src/services/devicesAPI.js
+++ src/services/devicesAPI.js
@@ -1,9 +1,9 @@
 export async function devicesApi() {
     try {
 
-       return await fetch("https://my-json-server.typicode.com/Jeck99/fake-server/devices")
-        .then((res) => res.json())
+        return await fetch("https://my-json-server.typicode.com/Jeck99/fake-server/devices")
+            .then((res) => res.json())
     } catch (error) {
-        
+
     }
 }
