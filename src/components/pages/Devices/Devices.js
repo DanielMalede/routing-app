026f0f6c5e22208ec26b48fdd6e2276e3c98968d
DanielMalede <danielmalede@gmail.com>
2022-08-28 15:57:25 +0300

    .

---
+++ src/components/pages/Devices/Devices.js
@@ -0,0 +1,10 @@
+import React    from "react";
+import template from "./Devices.jsx";
+
+class Devices extends React.Component {
+  render() {
+    return template.call(this);
+  }
+}
+
+export default Devices;
---
+++ src/components/pages/Devices/Devices.jsx
@@ -0,0 +1,12 @@
+import "./Devices.css";
+import React from "react";
+
+function template() {
+  return (
+    <div className="devices">
+      <h1>Devices</h1>
+    </div>
+  );
+};
+
+export default template;
