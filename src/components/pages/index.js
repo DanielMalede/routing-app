c81447330809c1e280e2dd0433a99310ca51ed1f
DanielMalede <danielmalede@gmail.com>
2022-10-07 14:13:25 +0300

    .

--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -1,10 +1,5 @@
-import NotFound from "./NotFound/NotFound"
-import Footer from "../features/Footer/Footer"
-import Home from './Home/Home'
-import Main from './Main/Main'
-export {
-    Home,
-    Main,
-    Footer,
-    NotFound,
-}
+import NotFound from "./NotFound/NotFound";
+import Footer from "../features/Footer/Footer";
+import Home from "./Home/Home";
+import Main from "./Main/Main";
+export { Home, Main, Footer, NotFound };
